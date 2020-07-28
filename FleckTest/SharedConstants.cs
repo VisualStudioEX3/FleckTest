0d178e87417f02f4a02a07f0142e62dbe2ff2762
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2020-07-28 02:32:25 +0200

    Removed unused constants.

--- FleckTest/SharedConstants.cs
+++ FleckTest/SharedConstants.cs
@@ -13,10 +13,6 @@ namespace FleckTest
 
         public const int SERVER_USERNAME_AVAILABLE          = 1000;
         public const int SERVER_USERNAME_IN_USE             = 1001;
-
-        public const int CLIENT_CONNECTION_OK               = 2000;
-        public const int CLIENT_CONNECTION_ERROR            = 2001;
-        public const int CLIENT_CANCELLED_LOGIN             = 2002;
         #endregion
     }
 }
